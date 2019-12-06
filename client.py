19ce3fc9ab60bd78a2976504bf7c48d84d2b4ed5
propene123 <benjamin.russell@durham.ac.uk>
2019-12-06 00:44:14 +0000

    comments

--- client.py
+++ client.py
@@ -3,106 +3,150 @@ import json
 from socket import *
 
 
+# function takes a socket and returns a tuple of server
+# response type and server response payload
 def parseResponse(socket):
     try:
         sentence = socket.recv(1024).decode()
+        # split response into expected length in bytes
+        # and json payload as string
         splitSentence = sentence.split('_', 1)
         length = int(splitSentence[0])
         message = splitSentence[1]
+        # check json payload is of expected length
         messageLength = len(message.encode('utf-8'))
         if(messageLength < length):
+            # receive rest of json payload
             message += socket.recv(length - messageLength).decode()
+        # parse json into dictionary
         obj = json.loads(message)
         return (obj['type'], obj['payload'])
+    # if the response is malformed inform the user
     except (IndexError, ValueError):
         return ('ERROR', 'Server sent malformed response')
     except (json.JSONDecodeError, KeyError):
         return ('ERROR', 'Server sent malformed response')
+    # inform the user if a timeout occurred
     except timeout:
         print('Server timed out')
         socket.close()
         exit()
+    # inform the user if the socket failed during the operation
     except (ConnectionError, OSError):
         print('The connection has been closed by the server')
         socket.close()
         exit()
 
 
+# function takes a socket and json paylaod as string
+# and encodes the message before sending it
 def sendMessage(socket, msg):
+    # calculate length of json string in bytes
     length = len(msg.encode('utf-8'))
+    # prepend json string with length
     message = str(length) + '_' + msg
     try:
+        # send the message
         socket.sendall(message.encode())
+    # notify user if the server is down
     except (ConnectionError, OSError):
         print('Server is down... Exiting Client')
         socket.close()
         exit()
 
 
+# function takes a socket and sends a GET_BOARDS message
 def getBoards(socket):
     msgType = 'GET_BOARDS'
+    # create json payload as a string
     msgJson = json.dumps({'type': msgType})
+    # pass msg type and payload to sendMessage
     sendMessage(socket, msgJson)
 
 
+# function takes a socket and sends a GET_MESSAGES message
 def getMessages(socket, boardNumber):
     msgType = 'GET_MESSAGES'
+    # create json payload as a string
     msgJson = json.dumps({'type': msgType, 'board': boards[boardNumber - 1]})
+    # pass msg type and payload to sendMessage
     sendMessage(socket, msgJson)
 
 
+# function takes a socket and sends a GET_MESSAGES message
 def postMessage(socket, boardNumber, title, content):
     msgType = 'POST_MESSAGE'
+    # create json payload as a string
     msgJson = json.dumps({'type': msgType,
                           'board': boards[boardNumber - 1],
                           'title': title,
                           'msg': content})
+    # pass msg type and payload to sendMessage
     sendMessage(socket, msgJson)
 
 
+# function takes a socket and sends a GET_BOARDS message
+# parses the response and parses it before printing a list
+# of boards to the screen. And returns new list of boards
 def refresh(socket):
     getBoards(socket)
     response = parseResponse(socket)
     respType = response[0]
     respPayload = response[1]
     boards = respPayload
+    # check if the response is an error
     if(respType == 'ERROR'):
+        # print the error and exit the client
         print(respPayload)
         socket.close()
         exit()
+    # print list of board names numbered in ascending order
     i = 1
     for board in respPayload:
         print(str(i) + '.', board.replace('_', ' '))
         i += 1
+    # return the new list of boards
     return boards
 
 
+# read command line args as server address and port
 serverAddress = sys.argv[1]
 serverPort = sys.argv[2]
+# create socket object
 clientSocket = socket(AF_INET, SOCK_STREAM)
+# attempt to establish a connection to server
 try:
     clientSocket.connect((serverAddress, int(serverPort)))
 except BaseException:
+    # if connection failed print to user and exit client
     print("Could not open requested server and port")
     exit()
+# ensure socket timesout after 10 seconds
 clientSocket.settimeout(10)
+# get list of boards
 boards = refresh(clientSocket)
+# main client loop
 while True:
+    # prompt user for input
     userInput = input(
         'Enter a board number to view lastest 100 messages from that board.'
         'Enter POST to post a message. Enter REFRESH to refresh list of boa'
         'rds. Enter QUIT to close the client\n')
+    # if input is digit check its a valid board entry
     if(userInput.isdigit()):
         index = int(userInput)
         if(index < 1 or index > len(boards)):
             print('Please enter a number on the list')
             continue
+        # get messages from the selected board
         getMessages(clientSocket, index)
         response = parseResponse(clientSocket)
+        # if response is an error print it
         if(response[0] == 'ERROR'):
             print(response[1])
         else:
             i = 0
+            # print titles with message content beneath title
             for title in response[1]['titles']:
                 print(
                     '*********************************************************'
@@ -113,27 +157,38 @@ while True:
             print(
                 '*********************************************************'
                 '********************************')
+    # if user enters POST prompt for post info
     elif(userInput.upper() == 'POST'):
         userInput = input('Enter number of board to post to\n')
+        # check the board number is an actual number
         if(not userInput.isdigit()):
             print('Input is not a number')
             continue
         index = int(userInput)
+        # check the board number is valid
         if(index < 1 or index > len(boards)):
             print('Please enter a number on the list')
             continue
+        # prompt user for message title and contents
         title = input('Enter message title\n')
         msgText = input('Enter message content\n')
+        # send the message to the client
         postMessage(clientSocket, index, title, msgText)
+        # wait for resposne
         response = parseResponse(clientSocket)
+        # if reponse is an error print to user
         if(response[0] == 'ERROR'):
             print(response[1])
         else:
+            # print success
             print(response[1])
+    # if user enters REFRESH get new list of boards
     elif(userInput.upper() == 'REFRESH'):
         boards = refresh(clientSocket)
+    # if user enters QUIT break out of main loop and exit
     elif(userInput.upper() == 'QUIT'):
         break
+    # print if user input is not a vaild option
     else:
         print('invalid input try again')
 
--- server.py
+++ server.py
@@ -6,42 +6,65 @@ from threading import Thread, Lock
 from datetime import datetime
 
 
+# function takes filepath, ip and port tuple, message type
+# and whether an operation was successful or not
 def writeToLog(path, address, messageType, success):
+    # lock mutex so only 1 thread can write to log at once
     with logLock:
         try:
             with open(path, 'a') as f:
                 try:
+                    # create 1 line of output
                     outLine = ''
+                    # output starts with client address and port
                     outLine += address[0] + ':' + str(address[1]) + '\t'
+                    # then the time and date of the request
                     outLine += datetime.today().strftime('%c') + '\t'
+                    # then the type of request
                     outLine += messageType + '\t'
+                    # finally whether or not the request was successful
                     if(success):
                         outLine += 'OK'
                     else:
                         outLine += 'ERROR'
                     outLine += '\n'
                     f.write(outLine)
+                # if the log file cannot be written to the notify user
+                # and exit server
                 except IOError:
                     print('Could not write to log file... exiting server')
                     exit()
+        # if the log file cannot be opened then print error and exit
+        # server
         except FileNotFoundError:
             print('Could not open log file... exiting server')
             exit()
 
 
+# function takes a socket, response type, and payload
+# and sends the response message to the client
 def sendMessage(socket, respType, payload):
+    # encodes respType and payload as json string
     response = json.dumps({'type': respType, 'payload': payload})
+    # calculate lenght of json string in bytes
     length = len(response.encode('utf-8'))
+    # prepend json string with length
     message = str(length) + '_' + response
+    # attempt to send resposne to client
     try:
         socket.sendall(message.encode())
+    # if the client has closed notify user
     except (ConnectionError, OSError):
         print('Connection was closed on a client thread')
 
 
+# function takes a socket gets a lsit of all subfolders of
+# boards folder and sends that to the client socket
 def getBoards(socket):
     payload = ''
     respType = ''
+    # returns list of everything in boards folder that is
+    # a directory
     payload = [
         x for x in os.listdir(
             os.path.join(
@@ -52,85 +75,127 @@ def getBoards(socket):
                     os.getcwd(),
                     'board'),
                 x))]
+    # if there are no boards send ERROR back to client
     if not payload:
         respType = 'ERROR'
         payload = 'No boards defined'
+        # write error to log file
         writeToLog(logPath, socket.getpeername(), 'GET_BOARDS', False)
     else:
+        # write success to log file
         respType = 'GET_BOARDS_RESPONSE'
         writeToLog(logPath, socket.getpeername(), 'GET_BOARDS', True)
+    # send response to client
     sendMessage(socket, respType, payload)
 
 
+# helper function to determine if a board exisits or not
 def boardExists(board):
     res = False
+    # get board as filepath
     boardPath = os.path.join(os.getcwd(), 'board', board)
+    # set result to True if board exists and is a file
     if os.path.exists(boardPath) and os.path.isdir(boardPath):
         res = True
+    # return result
     return res
 
 
+# function takes socket and board name
 def getMessages(socket, board):
     payload = ''
     respType = ''
+    # if board does not exist send Error to user
     if (not boardExists(board)):
         payload = 'Board does not exist'
         respType = 'ERROR'
+        # log ERROR
         writeToLog(logPath, socket.getpeername(), 'GET_MESSAGES', False)
     else:
+        # get board name as filepath
         boardPath = os.path.join(os.getcwd(), 'board', board)
+        # returns list of all files in board folder
         names = [os.path.splitext(x)[0] for x in os.listdir(
             boardPath) if os.path.isfile(os.path.join(boardPath, x))]
+        # sort filenames into descending order
         names.sort(reverse=True)
+        # take only the first 100 message files
         names = names[:100]
         messages = []
+        # for each message read its contents into another list
         for i in range(len(names)):
             filepath = os.path.join(os.getcwd(), 'board', board, names[i])
             with open(filepath) as f:
                 messages += f.readlines()
+            # edit list of message titles to not be the full filenames
             names[i] = names[i].split('-', 2)[2]
+        # create response payload
         payload = {'titles': names, 'messages': messages}
         respType = 'GET_MESSAGES_RESPONSE'
+        # log success
         writeToLog(logPath, socket.getpeername(), 'GET_MESSAGES', True)
+    # send response to client
     sendMessage(socket, respType, payload)
 
 
+# function takes socket, board, title, and message contents
 def postMessage(socket, board, title, msg):
     payload = ''
     respType = ''
+    # if the board does not exist send ERROR to client
     if (not boardExists(board)):
         payload = 'Board does not exist'
         respType = 'ERROR'
+        # log ERROR
         writeToLog(logPath, socket.getpeername(), 'POST_MESSAGE', False)
     else:
+        # create message filename. Start with current date and time
         filename = datetime.today().strftime('%Y%m%d-%H%M%S-')
+        # next the message title with spaces as underscores
         filename += title.replace(' ', '_')
+        # turn filename in filepath
         filepath = os.path.join(os.getcwd(), 'board', board, filename)
         try:
+            # open message file
             with open(filepath, 'w') as f:
+                # write message contents
                 f.write(msg)
+                # set response to success
                 respType = 'POST_MESSAGE_RESPONSE'
                 payload = 'Successfully posted message'
+                # log success
                 writeToLog(logPath, socket.getpeername(), 'POST_MESSAGE', True)
+        # if the file could not be written to or created then ERROR
         except OSError:
+            # set resposne to ERROR
             respType = 'ERROR'
             payload = 'Could not create message with that title'
+            # log ERROR
             writeToLog(logPath, socket.getpeername(), 'POST_MESSAGE', False)
+    # send response to client
     sendMessage(socket, respType, payload)
 
 
+# function takes a socket and sends an ERROR
 def unrecognisedMessage(socket):
+    # set response type to ERROR
     payload = 'That command is not a recognised command'
     respType = 'ERROR'
+    # send response
     sendMessage(socket, respType, payload)
 
 
+# function takes json string as msg and a socket
+# calls correct message handling function
 def handleMessage(msg, socket):
+    # test if msg is a valid json string
     try:
         obj = json.loads(msg)
     except json.JSONDecodeError:
+        # if not valid json inform client
         unrecognisedMessage(socket)
     try:
+        # based on type call correc handler function
         if(obj['type'] == 'GET_BOARDS'):
             getBoards(socket)
         elif(obj['type'] == 'GET_MESSAGES'):
@@ -139,50 +204,71 @@ def handleMessage(msg, socket):
             postMessage(socket, obj['board'], obj['title'], obj['msg'])
         else:
             unrecognisedMessage(socket)
+    # if no type field inform user of invalid message
     except KeyError:
         unrecognisedMessage(socket)
 
 
+# function takes socket and spawns a daemon thread
+# for the client on that socket
 def clientThread(socket):
+    # main client loop
     while True:
         try:
+            # attempt to read incoming client message
             sentence = socket.recv(1024).decode()
+            # if socket is closed break and terminate thread
             if not sentence:
                 print("Connection closed on client thread")
                 break
             try:
+                # attempt to split response into length and payload
                 splitSentence = sentence.split('_', 1)
                 length = int(splitSentence[0])
                 message = splitSentence[1]
+            # if error is thrown then message was invalid
             except (IndexError, ValueError):
+                # inform client of invalid message
                 unrecognisedMessage(socket)
             messageLength = len(message.encode('utf-8'))
+            # check all message has been received
             if(messageLength < length):
+                # receive rest of message
                 message += socket.recv(length - messageLength).decode()
+            # handle the message appropriately
             handleMessage(message, socket)
+        # if client has been closed inform user and end thread
         except (ConnectionError, OSError):
             print("Connection closed on client thread")
             break
     socket.close()
 
 
+# read commandline args as server address and port
 serverAddress = sys.argv[1]
 serverPort = sys.argv[2]
+# create server socket
 serverSocket = socket(AF_INET, SOCK_STREAM)
+# attempt to open server socket
 try:
     serverSocket.bind((serverAddress, int(serverPort)))
 except BaseException:
+    # if socket cant be opened inform user and exit
     print("Could not open requested server and port")
     exit()
+# set small timeout so server can be closed via keyboard interrupt
 serverSocket.settimeout(0.2)
 serverSocket.listen(5)
+# create log mutex and logpath
 logLock = Lock()
 logPath = os.path.join(os.getcwd(), 'server.log')
-
+# main server loop
 while True:
     try:
         try:
+            # attempt to gain a connection
             (connectionSocket, addr) = serverSocket.accept()
+            # pass client socket to new thread to be dealt with
             x = Thread(
                 target=clientThread,
                 daemon=True,
@@ -190,8 +276,10 @@ while True:
                     connectionSocket,
                 ))
             x.start()
+        # this exists so keyboard interrupts are possible
         except timeout:
             pass
+    # on keyboard interrupt exit server
     except KeyboardInterrupt:
         print('Keyboard interrupt... Closing server')
         break
